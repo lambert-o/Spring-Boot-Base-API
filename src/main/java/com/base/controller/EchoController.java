09c8d9613d22921044d6f7f44fb770ceb439fd59
Owen Lambert <OwenLambert98@hotmail.com>
2023-04-01 21:17:31 +0100

    Use ModelAttribute

--- src/main/java/com/base/controller/EchoController.java
+++ src/main/java/com/base/controller/EchoController.java
@@ -4,14 +4,14 @@ import com.base.domain.EchoRequest;
 import com.base.service.EchoService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
     @Operation(summary = "Echos a response")
     @GetMapping("/echo")
-    public String echo(@RequestParam("echo") EchoRequest echoRequest) {
+    public String echo(@ModelAttribute EchoRequest echoRequest) {
         EchoService echoService = new EchoService();
         System.out.println("Calling echo service");
         return echoService.echo(echoRequest);
