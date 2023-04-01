ee5b1bfc76bf779fa081539ad7bd195ae567fb3b
Owen Lambert <wackyowey@hotmail.co.uk>
2023-04-01 01:35:08 +0100

    Feat - include OpenAPI documentation

---
+++ src/main/java/com/base/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.base;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring-Boot-Base-API")
+                        .description("A base on which to develop an API using Spring")
+                        .version("1.0.0"));
+    }
+}
--- src/main/java/com/base/controllers/EchoController.java
+++ src/main/java/com/base/controllers/EchoController.java
@@ -2,12 +2,14 @@ package com.base.controllers;
 
 import com.base.domain.EchoRequest;
 import com.base.services.EchoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
+    @Operation(summary = "Echos a response")
     @GetMapping("/echo")
     public String echo(@RequestParam("echo") EchoRequest echoRequest) {
         EchoService echoService = new EchoService();
