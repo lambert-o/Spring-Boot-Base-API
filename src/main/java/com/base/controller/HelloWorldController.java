7625c8d47911d4f6002770bc94058fe8eceeedb4
Owen Lambert <OwenLambert98@hotmail.com>
2023-05-21 00:47:14 +0100

    Use lombok log4j2 implementation

--- src/main/java/com/base/controller/HelloWorldController.java
+++ src/main/java/com/base/controller/HelloWorldController.java
@@ -3,20 +3,19 @@ package com.base.controller;
 import com.base.domain.HelloRequest;
 import com.base.service.HelloService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class HelloWorldController {
-    private static final Logger logger = LogManager.getLogger(HelloWorldController.class);
     @Operation(summary = "Echos a response")
     @GetMapping("/hello")
     public String hello(@ModelAttribute HelloRequest helloRequest) {
         HelloService helloService = new HelloService();
-        logger.info("Calling hello service");
+        log.info("Calling hello service");
         return helloService.hello(helloRequest);
     }
 }
