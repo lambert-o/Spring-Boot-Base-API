ddf12241b5b6869f8705b7ec31c6f1486bc376e8
Owen Lambert <OwenLambert98@hotmail.com>
2023-05-21 00:34:42 +0100

    Implement log4j 2

--- src/main/java/com/base/controller/HelloWorldController.java
+++ src/main/java/com/base/controller/HelloWorldController.java
@@ -3,17 +3,20 @@ package com.base.controller;
 import com.base.domain.HelloRequest;
 import com.base.service.HelloService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    private static final Logger logger = LogManager.getLogger(HelloWorldController.class);
     @Operation(summary = "Echos a response")
     @GetMapping("/hello")
     public String hello(@ModelAttribute HelloRequest helloRequest) {
         HelloService helloService = new HelloService();
-        System.out.println("Calling hello service");
+        logger.info("Calling hello service");
         return helloService.hello(helloRequest);
     }
 }
