bf628c4334c3bd7e92dbb810c5595c8d63971154
Owen Lambert <OwenLambert98@hotmail.com>
2023-05-25 01:38:28 +0100

    Setup tests - WIP

---
+++ src/test/java/HelloServiceTest.java
@@ -0,0 +1,27 @@
+import com.base.domain.HelloRequest;
+import com.base.service.HelloService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class HelloServiceTest {
+
+    @InjectMocks
+    HelloService helloService;
+
+    @Test
+    void shouldReturnValidResponseWhenRequestValid() {
+        // given
+        HelloRequest request = new HelloRequest("Owen Lambert");
+
+        // when
+        var response = helloService.hello(request);
+
+        // then
+        assertEquals("Hello, Owen Lambert", response);
+    }
+}
