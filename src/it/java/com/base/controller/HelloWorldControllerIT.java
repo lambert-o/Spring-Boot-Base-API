24fdf424ebe3fc0a8d6d432a28ed795e430a6d2c
Owen Lambert <107768978+lambert-o@users.noreply.github.com>
2023-06-13 23:35:16 +0100

    Use in-memory DB for IT and add GitHub Actions CI (#15)
    * Use in-memory DB for IT and add GitHub Actions CI
    
    * Build and use test image

--- src/it/java/com/base/controller/HelloWorldControllerIT.java
+++ src/it/java/com/base/controller/HelloWorldControllerIT.java
@@ -1,11 +1,13 @@
 package com.base.controller;
 
+import com.base.configuration.TestConfig;
 import com.base.repository.MyDatabaseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,10 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = TestConfig.class)
 public class HelloWorldControllerIT {
-
-    @MockBean
-    MyDatabaseRepository database;
 
     @Autowired
     private MockMvc mockMvc;
